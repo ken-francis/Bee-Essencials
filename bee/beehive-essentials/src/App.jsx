e199d5114d5abac9e3315a8d3d51eca6abd0f68e
Kenfrancis <kennedynyoro1@gmail.com>
2025-11-22 08:56:49 +0000

    Connected Navigation + Hero in App.jsx

---
+++ bee/beehive-essentials/src/App.jsx
@@ -0,0 +1,14 @@
+import Navigation from './components/layout/Navigation';
+import Hero from './components/sections/Hero';
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Hero />
+      {/* We’ll add more sections one by one */}
+    </>
+  );
+}
+
+export default App;
